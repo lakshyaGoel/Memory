ee4e4a183712f0721a1c623a00071fa8e790d8f9
Nishka Monteiro <nishkamonteiro@gmail.com>
2017-12-03 15:44:57 -0600

    Basic Image select

--- mobile/screens/AddImage.js
+++ mobile/screens/AddImage.js
@@ -4,16 +4,55 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
 } from 'react-native';
-
+import { Constants, ImagePicker } from 'expo';
 
 export default class AddImage extends React.Component {
+  state = {
+    image: null,
+  };
 
+  _pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+    }
+  };
+  
+  _pickImageCam = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      this.setState({ image: result.uri });
+    }
+  };
   render() {
+    let { image } = this.state;
     
     return (
       <View style={styles.container}>
-        <Text>Show functionality to add a Image Note</Text>
+        <Button
+          title="Pick an image from camera roll"
+          onPress={this._pickImage}
+        />
+        <Button
+          title="Pick an image from camera roll"
+          onPress={this._pickImageCam}
+        />
+        {image &&
+          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
       </View>
     )
   }
