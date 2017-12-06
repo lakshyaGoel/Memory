5b1e6cd54887945a12c8f2180ce24075f6619aeb
reiven <seventhtrigger0@gmail.com>
2017-12-05 22:51:00 -0600

    [fix] reformat my part of client side code

--- mobile/screens/Notes.js
+++ mobile/screens/Notes.js
@@ -1,36 +1,36 @@
 import React from 'react';
 import {
-  Button,
-  StyleSheet,
-  Text,
-  View,
+    Button,
+    StyleSheet,
+    Text,
+    View,
 } from 'react-native';
 import Memory from './components/card';
 
-export default class Notes extends React.Component{
 
-  render() {
-    
-    return (
-      <View style={styles.container}>
-        <Text>Show All notes (My Notes + Shared Notes) with a toggle at the top to show only My Notes. Also, show a small icon if the note is a Shared Note</Text>
-        <Memory/>
-      </View>
-    )
-  }
+export default class Notes extends React.Component {
 
+    render(){
+        return (
+            <View style={styles.container}>
+                <Text>Show All notes (My Notes + Shared Notes) with a toggle at the top to show only My Notes. Also,
+                    show a small icon if the note is a Shared Note</Text>
+                <Memory/>
+            </View>
+        )
+    }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    marginTop: 40,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title: {
+        fontSize: 20,
+        textAlign: 'center',
+        marginTop: 40,
+    },
 });
